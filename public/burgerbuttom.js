eeaf72434ed29afc14474d1cf400c56153c49bbe
Dujardin fabrice <fabricedujardin873@gmail.com>
2021-01-02 19:05:05 +1100

    a fini popup

--- public/burgerbuttom.js
+++ public/burgerbuttom.js
@@ -1,3 +1,19 @@
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".open").on("click", function() {
+  $(".popup-overlay, .popup-content").addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+});
+
+
+
+$(".click1").on("click", function() {
+  $(".deleteproduct1, .col-11").addClass("hidden");
+});
